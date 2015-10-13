650fa1b54bd0657762efb73b39a98b7bf88d9fd9
David Gleba <dgleba@gmail.com>
2015-10-13 13:32:10 -0400

    2015-10-13_Tue_13.32-PM

---
+++ tmp1.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+
+
+
+
+get1
+{
+
+worked...
+curl --digest --user dgleba:x123za https://bitbucket.org/dgleba/vamp198e/get/master.zip -o vamp198e.zip
+
+192.168.88.86
+
+}
+
+#
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+saynow 
+{
+# this function not used.
+echo (date +"__%Y-%m-%d_%a_%k.%M.%S-%Z")
+}
+
+offlinestuff()  {
+
+echo tmp1
+
+}
+
+saynow
+get1
+#
+
