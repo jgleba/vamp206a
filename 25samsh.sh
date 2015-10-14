8e72bfec46964f6eea4910afa744711fa9d246d9
dgleba <dgleba@gmail.com>
2015-10-14 13:41:58 -0400

    2015-10-14_Wed_13.41-PM

--- 25samsh.sh
+++ 25samsh.sh
@@ -43,6 +43,17 @@ valid users = $userv
 # http://www.cyberciti.biz/tips/how-do-i-set-permissions-to-samba-shares.html
 # https://www.howtoforge.com/samba-server-ubuntu-14.04-lts
 #
+[rt]
+path = /
+browsable =yes
+writable = yes
+guest ok = no
+read only = no
+valid users = $userv
+# sudo smbpasswd -a $userv
+# http://www.cyberciti.biz/tips/how-do-i-set-permissions-to-samba-shares.html
+# https://www.howtoforge.com/samba-server-ubuntu-14.04-lts
+#
 EOF
 
 smbpasswd -a $userv%$a
