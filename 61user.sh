facf58a90f5115a96871f270a228f678da91c7a7
David Gleba <dgleba@gmail.com>
2015-10-18 17:59:56 -0400

    useradd

--- 61user.sh
+++ 61user.sh
@@ -5,9 +5,9 @@
 set -x
 date
 
-sudo useradd webed
+sudo useradd dgleba
 sudo groupadd www
-sudo useradd -G www webed
+sudo useradd -G www dgleba
 
 chgrp -hR www /var/www/html
 chmod -R g+rw  /var/www/html
