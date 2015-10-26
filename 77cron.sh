8a1da793a41251e1d9a35d1d619cb847f1d88f8c
dgleba <dgleba@gmail.com>
2015-10-26 13:38:23 -0400

    cron

---
+++ 77cron.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+cd
+set -vx
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+function info()
+{
+exit 999
+
+cron know..
+
+http://unix.stackexchange.com/questions/119481/how-come-crontab-e-is-different-from-less-etc-crontab
+
+http://unix.stackexchange.com/questions/119481/how-come-crontab-e-is-different-from-less-etc-crontab
+
+
+/var/spool/cron/crontabs/albe
+
+
+}
+#
