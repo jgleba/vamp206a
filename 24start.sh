7fe9e66661060e6cf4c3d053fe072ca6f24ba490
dgleba <dgleba@gmail.com>
2015-10-28 12:39:40 -0400

    m

--- 24start.sh
+++ 24start.sh
@@ -11,19 +11,33 @@ source shc/21env.sh
 #comment this out for full auto run...
 #exit 0
 
+cd
 source shc/root.sh
+cd
 source shc/15samsh.sh
+cd
 source shc/17docs.sh
+cd
 source shc/31base.sh
+cd
 source shc/34samm.sh
+cd
 source shc/43apach.sh
+cd
 #source shc/47guestad.sh
+cd
 source shc/51dj.sh
+cd
 source shc/53imp.sh
+cd
 source shc/61user.sh
+cd
 source shc/63bauth.sh
+cd
 source shc/65cron.sh
+cd
 source shc/67hostn.sh
+cd
 
 read -t  99 -p "Hit ENTER or wait about 99 seconds" ; echo ;
 
