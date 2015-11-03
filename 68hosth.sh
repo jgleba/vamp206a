f3e7bc800d7420e69bc0bc58f59f62700d4d19ee
dgleba <dgleba@gmail.com>
2015-11-03 16:05:30 -0500

    m

--- 68hosth.sh
+++ 68hosth.sh
@@ -23,7 +23,7 @@ cat /etc/hostname
 sudo tee /etc/hosts <<EOF
 #
 127.0.0.1	localhost
-127.0.1.1	pmdsdata3.local	pmdsdata9t
+127.0.1.1	pmdsdata3.local	pmdsdata3
 
 # The following lines are desirable for IPv6 capable hosts
 ::1     localhost ip6-localhost ip6-loopback
