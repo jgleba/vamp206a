f8711a972030191eb5e525f40a98429b1d0dbfe9
dgleba <dgleba@gmail.com>
2015-11-06 10:17:15 -0500

    m

--- nuser.sh
+++ nuser.sh
@@ -2,9 +2,15 @@
 date
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # add new user. 
-echo usage nuser.sh  username 
+echo Usage nuser.sh  username 
 echo
-read -ep " lease enter the new users password: " pwnu1
+
+if [ $# -eq 0 ]; then
+    echo "No arguments provided"
+    exit 1
+fi
+
+read -ep "Please enter the new users password: " pwnu1
 nuser="$1"
 echo user: "$nuser"
 echo $pwnu1
@@ -14,8 +20,10 @@ echo "$nuser:$pwnu1" | sudo chpasswd
 #add user to group www
 sudo usermod -a -G www  $nuser
 (echo "$pwnu1"; echo "$pwnu1") | sudo smbpasswd -s -a $nuser
+sudo mkdir /home/$nuser/bin
+sudo chown  $nuser:$nuser /home/$nuser/bin
 echo groups:
-groups 
+groups $nuser
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #
 date
