f9f6358b59837d119169d823bef0d4f981fc17a4
David Gleba <dgleba@gmail.com>
2015-12-14 22:13:59 -0500

    host parameter

--- 100/163mullvad.sh
+++ 100/163mullvad.sh
@@ -14,22 +14,10 @@ exit 999
 : <<'END'
 
 comment content
-ls
-
-END
-# end block comment ===============================
-}
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-#
-date
-#
-
+=
+gui is difficult to start. it doesn't easily show..
 #mullvad
-
 sudo apt-get update
-
-
 cd
 mkdir -p 09
 cd 09
@@ -45,4 +33,83 @@ sudo apt-get -y install python-qt4 pyqt4-dev-tools
 #sudo apt-get install lubuntu-desktop
 # this works, but wish i didn't have to install ubuntu desktop
 #sudo apt-get install ubuntu-desktop
+
+=
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+Title:  .
+-----------------------2015-12-14[Dec-Mon]21-37PM
+
+Logotype Mullvad MULLVAD
+DOWNLOAD
+FEATURES
+FAQ
+NEWS
+ LOG OUT
+OPENVPN
+
+Account number: 369959095035
+Time left: 5 days 
+OpenVPN configuration: mullvadconfig.zip
+
+Windows
+
+Install OpenVPN
+Extract the configuration files into %ProgramFiles%\OpenVPN\config\
+Start OpenVPN as Administrator (right-click, Run as ...)
+Right-click on the OpenVPN icon and choose "Connect"
+Mac
+
+Install the OpenVPN distribution Tunnelblick
+Extract the configuration files and double click Mullvad.tblk in the subdirectory OSX
+Select "Connect Mullvad" from the Tunnelblick menu
+Linux (Debian/Ubuntu)
+
+Option 1: a regular user
+
+Install network-manager-openvpn (sudo apt-get install network-manager-openvpn)
+Extract the configuration files
+Import mullvad_linux.conf using the Network Manager menu
+Option 2: root
+
+Install OpenVPN and resolvconf (sudo apt-get install openvpn resolvconf)
+Extract the configuration files into /etc/openvpn/
+Start with sudo /etc/init.d/openvpn start
+Select exit country
+
+If you want to choose where your device will seem to be located you can download a configuration file which will connect to a certain country:
+
+Netherlands	Germany	Sweden	Canada	USA
+MULLVAD – a VPN service that helps keep your online activity, identity and location private. GET STARTED 
+ABOUT
+TERMS
+FAQ
+DOWNLOAD
+NEWS
+SUPPORT
+(PUBLIC PGP KEY)
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+END
+# end block comment ===============================
+}
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#
+date
+#
+
+
+_____________
+
+
+#again..
+#https://wiki.archlinux.org/index.php/Mullvad
+
+sudo apt-get install openvpn resolvconf
+
+
+
+
 
---
+++ 68hostp.sh
@@ -0,0 +1,69 @@
+#!/usr/bin/env bash
+
+cd
+# echo off
+set +vx
+# echo on
+set -vx
+
+#purpose...
+#set hostname with   ** paramter **  usage 68hostp.sh [hostname2set]
+
+
+export hostv="$1"
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+sudo cp /etc/hosts /etc/hosts.bak.$(date +"__%Y-%m-%d_%a_%k.%M.%S-%Z")
+sudo cp /etc/hosts /etc/hosts.$(date +"%s").bak
+sudo cp /etc/hosts /etc/hosts.$(date "+%Y-%m-%d_%s").bak
+sudo cp /etc/hostname /etc/hostname.bak.$(date +"__%Y-%m-%d_%a_%k.%M.%S-%Z")
+
+
+sudo echo $hostv | sudo tee /etc/hostname
+cat /etc/hostname
+
+
+sudo tee /etc/hosts <<EOF
+#
+127.0.0.1	localhost
+#127.0.1.1	$hostv.local $hostv.stackpole.ca	$hostv
+127.0.1.1	$hostv.local 	$hostv
+
+# The following lines are desirable for IPv6 capable hosts
+::1     localhost ip6-localhost ip6-loopback
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+#
+EOF
+
+
+sudo cp /etc/ssmtp/ssmtp.conf /etc/ssmtp/ssmtp.conf.$(date +"%Y-%m-%d_%s").bak
+
+#working on --  hostname=
+nowdg1=`date +'__%Y-%m-%d_%a_%k.%M.%S-%Z'`
+sudo sed -i "/hostname=/i # \n# David Gleba kdg54 $nowdg1 ...\n#"  /etc/ssmtp/ssmtp.conf
+sudo sed -i "s/.*hostname=.*/hostname=$hostv.local/g" /etc/ssmtp/ssmtp.conf
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+function offline()
+{
+exit 999
+
+# begin block comment =============================
+: <<'END'
+#begin block comment --  http://stackoverflow.com/questions/947897/block-comments-in-a-shell-script
+ls
+END
+# end block comment ===============================
+}
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#
+date
+#
+
