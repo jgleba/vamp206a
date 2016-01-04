82fd9f384b13ccbc2320e287a728accb1c4eada4
dgleba <dgleba@gmail.com>
2016-01-04 10:11:05 -0500

    m

---
+++ 100/171blank.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+cd
+# echo off
+set +vx
+# echo on
+set -vx
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#put code here...
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function offline()
+{
+exit 999
+# begin block comment =============================
+: <<'END'
+
+comment content
+ls
+
+END
+# end block comment ===============================
+}
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#
+date
+#
+
