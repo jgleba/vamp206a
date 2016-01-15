55b1463931741319883be185be3f26e18fc43459
dgleba <dgleba@gmail.com>
2016-01-15 09:23:11 -0500

    m

--- apps/flask213/flask213e.sh
+++ apps/flask213/flask213e.sh
@@ -8,8 +8,9 @@ function Purpose() {
 
 ref:
 https://github.com/petrus-jvrensburg/flask-admin-examples
-
 http://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-xvii-deployment-on-linux-even-on-the-raspberry-pi
+
+shc/apps/flask213/flask213e.sh
 
 END
 # end block comment ===============================
@@ -20,6 +21,34 @@ cd ; date ; set +vx  ; set -vx ; # echo off, then echo on
 
 #main...
 
+
+# flask admin
+#   https://github.com/flask-admin/Flask-Admin/tree/master/examples/sqla
+
+mkdir -p /var/www/html/python/
+cd       /var/www/html/python/
+
+git clone https://github.com/flask-admin/flask-admin.git flask213e
+cd flask213e
+
+sudo pip install -r 'examples/sqla/requirements.txt'
+
+# the app throws an error when it is called app.py, rename it...
+#file "/var/www/html/python/apache/flask213e.wsgi", line 5, in <module> [:error] [pid 6602] from app import app as application [:error] ImportError: No module named app
+mv /var/www/html/python/flask213e/examples/sqla/app.py /var/www/html/python/flask213e/examples/sqla/app1.py
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# redirect to 918 if "flask213e/" is matched. so localhost/flask213e/ redirects to: localhost:917
+#
+cat <<EOF > /var/www/html/python/flask213e/examples/sqla/.htaccess
+RewriteEngine on
+#
+# redirect to 918 if "flask213e/" is matched
+RewriteCond %{SERVER_PORT} !^918$
+RewriteRule ^flask213e/(.*[^/])/?$ http://%{HTTP_HOST}:918/$1/ [R=301,L]
+#
+EOF
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 #deployment
@@ -40,37 +69,10 @@ source ~/shc/52vhost3.sh
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-
-cat <<EOF > /var/www/html/python/flask213e/examples/sqla/.htaccess
-RewriteEngine on
-#
-# redirect to 918 if "flask213e/" is matched
-RewriteCond %{SERVER_PORT} !^918$
-RewriteRule ^flask213e/(.*[^/])/?$ http://%{HTTP_HOST}:918/$1/ [R=301,L]
-#
-EOF
-
-
-#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-
-# flask admin
-#   https://github.com/flask-admin/Flask-Admin/tree/master/examples/sqla
-
-mkdir -p /var/www/html/python/
-cd       /var/www/html/python/
-
-git clone https://github.com/flask-admin/flask-admin.git flask213e
-cd flask213e
-
-sudo pip install -r 'examples/sqla/requirements.txt'
-
-# the app throws an error when it is called app.py, rename it...
-#file "/var/www/html/python/apache/flask213e.wsgi", line 5, in <module> [:error] [pid 6602] from app import app as application [:error] ImportError: No module named app
-mv /var/www/html/python/flask213e/examples/sqla/app.py /var/www/html/python/flask213e/examples/sqla/app1.py
-
+# start dev server . visit localhost:5000
 python /var/www/html/python/flask213e/examples/sqla/app1.py
 
+#show apache log..
 sudo tail /var/log/apache2/error.log
 
 #
