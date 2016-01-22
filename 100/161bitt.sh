a559273161ef2c762d8c320f73b7885385be2c8b
David Gleba <dgleba@gmail.com>
2016-01-22 13:42:22 -0500

    m

--- 100/161bitt.sh
+++ 100/161bitt.sh
@@ -7,10 +7,13 @@ set -vx
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-
-
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 #bittorrent
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 
 #sudo add-apt-repository ppa:transmissionbt/ppa
 #sudo apt-get -y install transmission-cli transmission-common transmission-daemon
@@ -18,13 +21,33 @@ set -vx
 #sudo apt-get -y purge transmission-cli transmission-common transmission-daemon
 
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
 sudo apt-get -y install deluge
+# comment..
+# scheduler: you have to cleck each square respresenting an hour. it is positive and solid. You have to change the scheduled speed to change it. again it is positive not loosy goosey.
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 
 
 #qbittorrent
+#
+# works ok but...
+# qbitttorrent is ok, but when you manually switch from scheduled to other speed, it unchecks the schedule never to use it again. that's not ideal.
+# you have to go into settings and enable the schedule  again.
+#
 sudo add-apt-repository ppa:qbittorrent-team/qbittorrent-stable
 sudo apt-get update
 sudo apt-get -y install qbittorrent
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
 
 
 popcorn-time.se
@@ -42,6 +65,9 @@ albe@redekmatev2:~/bin$ ./Popcorn-Time
 [18483:0104/193620:WARNING:simple_index_file.cc(338)] Could not map Simple Index file.
 [18483:0104/193620:INFO:simple_index_file.cc(437)] Simple Cache Index is being restored from disk.
 ----
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 
