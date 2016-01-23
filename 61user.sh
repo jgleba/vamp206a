a791971704d7a1d4634364e24abcd952eef7fbf7
David Gleba <dgleba@gmail.com>
2016-01-22 20:27:58 -0500

    add sticky bit to preserve group in html

--- 61user.sh
+++ 61user.sh
@@ -22,10 +22,12 @@ source shc/21env.sh
 # set access to html files here..  [chmod chown chgrp]
 # sudo groupadd www  # just use www-data group
 # sudo chgrp -hR www /var/www/html
+#
 # chown and chgrp the html files appropriately.. chgrp -hR www-data /var/www/html
-sudo chgrp -hR www-data /var/www/html
 sudo chown -R root /var/www/html
-sudo chmod -R g+rw  /var/www/html
+sudo chgrp -hR www-data /var/www/html
+# also set the group sticky bit, so that the group is set for new files created. chmod g+s /home/shared – jris198944 May 13 '14 at 8:43 
+sudo chmod -R g+rws  /var/www/html
 sudo chmod -R o-rw /var/www/html
 # make only folders +x so they can be cd into.
 sudo find /var/www/html -type d -exec chmod g+x {} +
