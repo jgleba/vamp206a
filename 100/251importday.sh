1059987f8c44c21131dcba106bfa02123ee2b5ac
dgleba <dgleba@gmail.com>
2016-02-01 13:58:27 -0500

    m

---
+++ 100/251importday.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+echo starting 251importday.sh  - this imports fresh data daily...
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#
+dgmethod="m1"
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+date
+set -x
+cd
+source shc/21env.sh
+
+# make directories. # html
+sudo chmod -R 777 tmp01
+sudo mkdir -p /var/www/html
+
+#
+# make tempfiles folder writable. This is used with cilist, tnlist and others to write excel files and mail them..
+#
+sudo mkdir -p /tmp01/tempfiles
+sudo chmod -R 777 /tmp01/tempfiles
+ 
+
+if [ $dgmethod = "m1" ] ; then
+
+ 
+        if ping -c 1 192.168.88.64 &> /dev/null
+        then
+          curl -O -u dg:fruit ftp://192.168.88.94:2141//files/htdocs.PMDS-DATA.latest.7z
+        else
+              read -t 33 -p "Oops, cannot ping redek210.  Press Enter." ; echo ;
+            #exit 8
+        fi
+        
+        cd /home/$userv/tmp01
+        sudo rm -rf htdocs
+        read -t 10 -p "Hit ENTER or wait about ten seconds" ; echo ;
+        cd /home/$userv/tmp01
+        7z x /home/$userv/tmp01/htdocs.PMDS-DATA.latest.7z
+        source ~/shc/21env.sh
+        sudo rsync -vrltgoD /home/$userv/tmp01/htdocs/  /var/www/html
+  
+fi
+
+cd
+
+source shc/53imp.sh
+date
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+function offline()
+{
+exit 999
+
+cd 
+  curl -O -u dg ftp://108.168.7.195:46237//home/dg/www/Dropbox/csd/serve/vboxyard/htdocs/htdocs.PMDS-DATA.latest.7z
+
+}
