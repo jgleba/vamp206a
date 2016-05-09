ac586cd7ca3463b2982d322c8b37110269f713cf
David Gleba <dgleba@gmail.com>
2016-05-08 23:42:02 -0400

    first rails deploy

---
+++ 100/321hostnameboot.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function Purpose() {
+# begin block comment =============================
+: <<'END'
+
+#  Purpose:  
+
+END
+# end block comment ===============================
+}
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+cd ; date ; set +vx  ; set -vx ; # echo off, then echo on
+#
+
+# Main: put code here...
+
+
+
+#i want to make the hostname different on each boot so it won't conflict with another instance.
+
+
+
+
+#
+date
+#
