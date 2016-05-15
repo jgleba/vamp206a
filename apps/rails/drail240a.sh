284ad6622fffb9128aec2596ef47fb6fb276f8de
David Gleba <dgleba@gmail.com>
2016-05-15 12:34:13 -0400

    drail240a

--- apps/rails/drail240a.sh
+++ apps/rails/drail240a.sh
@@ -114,7 +114,76 @@ esac
 #
 EOF
 
+#Update the script's permissions and enable Unicorn to start on boot:
+sudo chmod 755 /etc/init.d/unicorn_drail240a
+sudo update-rc.d unicorn_drail240a defaults
 
+sudo apt-get -y install nginx
+
+
+
+sudo tee /etc/nginx/sites-available/drail240a.conf <<EOF
+#
+upstream app {
+    # Path to Unicorn SOCK file, as defined previously
+    server unix:/srv/web/drail240a/shared/sockets/unicorn.sock fail_timeout=0;
+}
+
+server {
+    listen 1222;
+    server_name localhost;
+
+    root /srv/web/drail240a/public;
+
+    try_files $uri/index.html $uri @app;
+
+    location @app {
+        proxy_pass http://app;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header Host $http_host;
+        proxy_redirect off;
+    }
+
+    error_page 500 502 503 504 /500.html;
+    client_max_body_size 4G;
+    keepalive_timeout 10;
+}
+#
+EOF
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+#change nginx      default port     to 82 so apache still works on 80..
+
+#sudo leafpad /etc/nginx/sites-enabled/default
+# orig settings...
+#    listen 80 default_server;
+#    listen [::]:80 default_server ipv6only=on;
+
+# backup..
+sudo cp  /etc/nginx/sites-enabled/default  /etc/nginx/sites-enabled/default.$(date "+%Y-%m-%d_%s").bk
+sudo cp  /etc/nginx/sites-available/default  /etc/nginx/sites-available/default.$(date "+%Y-%m-%d_%s").bk
+# remove line containing  '?????'  and replace the line completely with the new text...
+nowdg1=`date +'__%Y-%m-%d_%a_%H.%M.%S-%Z'`
+sudo sed -i "/.*listen 80 .*/i # \n# David Gleba kdg54 $nowdg1 ...\n#"  /etc/nginx/sites-enabled/default # add marker above the change.
+#Use double quotes to make the shell expand variables while keeping whitespace:
+sudo sed -i "s/.*listen 80 .*/       listen 82 default_server;/g" /etc/nginx/sites-enabled/default
+sudo sed -i "s/.*80 default_server.*/       listen [::]:82 default_server ipv6only=on;/g" /etc/nginx/sites-enabled/default
+# cat /etc/nginx/sites-enabled/default
+# move unneeded config out of the way...
+sudo mkdir /etc/nginx/offlinedg
+#sudo mv  /etc/nginx/sites-available/default /etc/nginx/offlinedg/default
+sudo mv /etc/nginx/sites-enabled/*.bak /etc/nginx/offlinedg
+sudo mv /etc/nginx/sites-enabled/*.bk /etc/nginx/offlinedg
+
+sudo service nginx stop
+sleep 1
+sudo service nginx start
+sudo service apache2 restart
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+sudo service nginx restart
 
 #
 date
