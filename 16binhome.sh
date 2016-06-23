7f14aa5252083a7a7465a303c040b9da1dce4efc
dgleba <dgleba@gmail.com>
2016-06-23 13:16:47 -0400

    m

--- 16binhome.sh
+++ 16binhome.sh
@@ -20,19 +20,6 @@ mkdir -p /home/$userv/backup
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 cp /home/$userv/bin/tarc /home/$userv/backup/tarc.$(date +"__%Y.%m.%d_%H.%M.%S").bk
-#
-cat <<EOF >> /home/$userv/bin/tarc
-###### tar current folder..
-mkdir -p tmp/dglog
-mv log/development.log tmp/dglog/development.log.$(date +"__%Y.%m.%d_%H.%M.%S").log
-###### tar current folder..
-base=$(basename $PWD);export HOSTNAME;cd ..
-tar -czf $base-$HOSTNAME$(date +"__%Y.%m.%d_%H.%M.%S").tgz  $base --exclude={$base/tmp,$base/vendor/assets/javascripts,$base/shared,$base/nbproject,$base/log};cd $base
-
-EOF
-#
-chmod +x /home/$userv/bin/tarc
-
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -44,7 +31,19 @@ EOF
 #
 chmod +x /home/$userv/bin/ifrs
 
+#
+cat <<EOF >> /root/bin/ifrs
+###### restart network eth0..
+sudo ifdown eth0 && sudo ifup eth0
+EOF
+#
+chmod +x /root/bin/ifrs
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+cp -rf shc/bin/* /home/$userv/bin
+chmod +x /home/$userv/bin/*
+
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
