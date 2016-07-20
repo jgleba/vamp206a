7af938f3e7ea95f46e834e1ec92174dbbc21fa86
dgleba <dgleba@gmail.com>
2016-07-20 13:59:20 -0400

    rails

--- rails/railsinstall-3step3.sh
+++ rails/railsinstall-3step3.sh
@@ -32,6 +32,32 @@ cd ; date ; set +vx  ; set -vx ; # echo off, then echo on
 # Main: put code here...
 
 
+####   !!!!   The following must be done as non-root....
+
+# i think this has to be done after exec $shell
+
+git clone https://github.com/rbenv/rbenv-gem-rehash.git ~/.rbenv/plugins/rbenv-gem-rehash
+
+#rbenv install 2.1.4
+#rbenv global 2.1.4
+# 2016-05-10 i think 2.3.0 will be ok next time... 
+rbenv install 2.3.0
+rbenv global 2.3.0
+
+ruby -v
+
+gem install bundler
+rbenv rehash
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+gem install rails
+
+gem install uglifier
+#http://stackoverflow.com/questions/34420554/there-was-an-error-while-trying-to-load-the-gem-uglifier-bundlergemrequire
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 #
 date
 #
