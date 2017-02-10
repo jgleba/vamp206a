d3857dd29416351ef92da8519d7ee75ed4670860
dgleba <dgleba@gmail.com>
2017-02-10 14:25:43 -0500

    2017-02-10pre

---
+++ tools/blank.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+saynow()
+{
+date1=$(date +"__%Y.%m.%d_%H.%M.%S")
+echo $date1
+timeout1=5 ; read -t "${timeout1}" -p "Press ENTER or wait $timeout1 seconds..." || true ;  echo ;
+}
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+function offline21() {
+
+echo tmp1
+exit 999
+
+: <<'BLOCKCOMMENT'
+
+  Purpose:   
+
+
+BLOCKCOMMENT
+}
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+#main...
+saynow
+#
+
+
