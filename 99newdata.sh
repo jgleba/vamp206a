17f130d615b27dac0cddb80b99961b1f48634b69
David Gleba <dgleba@gmail.com>
2017-09-12 20:03:10 -0400

    2017-09-12f

--- 99newdata.sh
+++ 99newdata.sh
@@ -12,9 +12,12 @@ END
 # end block comment ===============================
 }
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 cd
 source shc/21env.sh
 cd ; date ; set +vx  ; set -vx ; # echo off, then echo on
+set -vx
+
 #
 # Main: put code here...
 
@@ -47,19 +50,19 @@ cd tmp01
         #
 
         source ~/shc/21env.sh
-        sudo rsync -vrltgoD /home/$userv/tmp01/html/  /var/www/html
+        rsync -vrltgoD /home/$userv/tmp01/html/  /var/www/html
         cd
         #source shc/11get.sh
         cd
         source shc/53imp.sh
 
-        sudo chown -R root /var/www/html
-        sudo chgrp -hR www-data /var/www/html
-        # also set the group sticky bit, so that the group is set for new files created. chmod g+s /home/shared – jris198944 May 13 '14 at 8:43
-        sudo chmod -R g+rws  /var/www/html
-        sudo chmod -R o-rw /var/www/html
-        # make only folders +x so they can be cd into.
-        sudo find /var/www/html -type d -exec chmod g+x {} +
+#        sudo chown -R root /var/www/html
+#        sudo chgrp -hR www-data /var/www/html
+#        # also set the group sticky bit, so that the group is set for new files created. chmod g+s /home/shared – jris198944 May 13 '14 at 8:43
+#        sudo chmod -R g+rws  /var/www/html
+#        sudo chmod -R o-rw /var/www/html
+#        # make only folders +x so they can be cd into.
+#        sudo find /var/www/html -type d -exec chmod g+x {} +
 
 
         echo Data imported !!!!
@@ -75,7 +78,9 @@ cd tmp01
 #cd
 #source shc/52-v/52vh923.sh
 
+
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 # set access to html files here..  [chmod chown chgrp]
 # chown and chgrp the html files appropriately.. chgrp -hR www-data /var/www/html
 
